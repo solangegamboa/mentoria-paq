49bc8db62617e61e4803fa4895f4bed5dceb667c
Sol Gamboa <solange.gamboa@dentalspeed.com>
2023-10-25 17:39:37 -0300

    comentários

--- todo-list.alice/script.js
+++ todo-list.alice/script.js
@@ -2,6 +2,19 @@ const novaTarefa = document.querySelector ('.nova-tarefa');
 const lista = document.querySelector ('.lista');
 const botaoAdd = document.querySelector ('.btn-add-tarefa');
 
+/**
+ * Comentários gerais
+ * Melhorias:
+ *  Na função criaLi, retornar diretamente sem precisar de constante
+ *  Criar função de ação para quando clicar no checkbox (riscar o texto do item)
+ *  Criar função para deletar a linha (eliminar completamente o item)
+ * 
+ * Lembrar de usar IDs por linha (li) para ficar fácil de idetificar.
+ * 
+ */
+
+
+
 function criaLi(){
     const li = document.createElement('li');
     return li;
