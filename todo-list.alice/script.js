5584a5e0785a5eb8a15d4186112d1d25cabbefcc
Alice Silveira <alice.sturlese.silveira@gmail.com>
2023-10-31 10:20:59 -0300

    modificações
    coloquei o botão de apagar e a configuração da checkbox.

--- todo-list.alice/script.js
+++ todo-list.alice/script.js
@@ -1,6 +1,7 @@
 const novaTarefa = document.querySelector ('.nova-tarefa');
 const lista = document.querySelector ('.lista');
 const botaoAdd = document.querySelector ('.btn-add-tarefa');
+const caixaAdd = document.querySelector ('.caixaAdd');
 
 function criaLi(){
     const li = document.createElement('li');
@@ -10,16 +11,57 @@ function criaLi(){
 function criaTarefa(textoinput){
     const li = criaLi();
     li.innerText = textoinput;
+    const checkbox = document.createElement('input');//
+    checkbox.type = 'checkbox';//
+    li.prepend(checkbox);//
+    const label = document.createElement('label');
+    label.appendChild(checkbox);
+    label.appendChild(document.createTextNode(textoinput));
     lista.appendChild(li);
+    li.appendChild(label);
     limpaTarefa();
-
+    apagarTarefa(li);
+    adicionarCaixa(li);
+    
 }
 
 function limpaTarefa(){
     novaTarefa.value ='';
 }
 
-botaoAdd.addEventListener('click' , function(){
+//criando o botão apagar
+function apagarTarefa(li){
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'apagar';
+    botaoApagar.setAttribute('class' , 'apagar');
+    li.appendChild(botaoApagar);
+}
+
+    //botão add tarefa
+    botaoAdd.addEventListener('click' , function(){
     if (!novaTarefa.value) return;
     criaTarefa(novaTarefa.value);
-});
+    });
+
+    //apagar a tarefa
+    document.addEventListener('click' , function(e){
+        const el = e.target;
+        if (el.classList.contains('apagar')){
+        el.parentElement.remove();
+    }
+    });
+
+    function adicionarCaixa(tarefa) {
+        // Acesse a tarefa recém-criada aqui
+        const checkbox = tarefa.querySelector('input[type="checkbox"]');
+        if (checkbox) {
+            checkbox.checked = true;
+        }
+    }
+       
+
+
+
+
+//como adicionar a checkbox quando uma nova tarefa for criada++
